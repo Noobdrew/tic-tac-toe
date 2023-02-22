24f5b9eee30e6775c3fb99bdd1d65cbb88133b92
Ivan Marinski <noobdrew23@gmail.com>
2023-02-22 16:45:34 +0200

    made some module variables

---
+++ script.js
@@ -0,0 +1,31 @@
+//for(n=0;3n+1<=7;n++) 1st column
+//for(n=0;3n+2<=8;n++) 2nd column
+//for(n=0;3n+3<=9;n++) 3rd column
+
+//for(n=0;n+1<=3;n++) 1st row
+//for(n=0;n+4<=6;n++) 2nd row
+//for(n=0;n+7<=9;n++) 3rd row
+
+//special case 1,5,9 and 3,5,7
+
+
+//display controller module
+const displayController = (() => {
+    //all dom element manupulation is done here
+    const player2 = document.querySelector('.player2')
+    const cells = document.getElementsByClassName('cell')
+    const reset = document.querySelector('reset')
+    
+
+})();
+
+//gameboard module
+const gameBoard = (() => {
+    //store game state in an array and do checks for end of game
+    const gameArray = []
+
+})();
+
+
+
+
